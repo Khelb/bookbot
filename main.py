8621944f4999cb7fda83bebedc538720b1061a67
Khelb <jkoele@gmail.com>
2025-12-03 20:30:09 -0600

    feat: add book text reading and printing functionality
    - Add .gitignore to exclude 'books/' directory
    - Replace greeting with get_book_text function to read file contents
    - Add main function to print contents of 'books/frankenstein.txt'
    - Scaffold count_words function for future word counting feature

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-print("greetings boots")
+def get_book_text(file):
+    with open(file) as f:
+        file_data = f.read()
+    return file_data
+
+
+def main():
+    file_contents = get_book_text("books/frankenstein.txt")
+    print(file_contents)
+
+def count_words():
+
+
+main()
