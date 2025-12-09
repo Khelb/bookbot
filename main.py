9405fc9ee148a1704c4c94c65a6314379af2ccf1
Khelb <jkoele@gmail.com>
2025-12-08 20:39:39 -0600

    feat: allow specifying book path via command line argument
    Previously, the script analyzed a hardcoded book file. Now, users can provide the path to the book file as a command line argument, improving flexibility and usability. The script prints usage instructions if no path is provided.

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import sys
+
 from stats import character_counter, count_words, sorted_char_counter
 
 
@@ -8,22 +10,27 @@ def get_book_text(file):
 
 
 def main():
-    opened_file = get_book_text("books/frankenstein.txt")
-    split_string = count_words(opened_file)
-    counted_characters = character_counter(opened_file)
-    # print(counted_characters)
-    sorted_list = sorted_char_counter(counted_characters)
-    print("============ BOOKBOT ============")
-    print("Analyzing book found at books/frankenstein.txt...")
-    print("----------- Word Count ----------")
-    print(f"Found {split_string} total words")
-    print("--------- Character Count -------")
+    if len(sys.argv) < 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    else:
+        book_path = sys.argv[1]
+        opened_file = get_book_text(book_path)
+        split_string = count_words(opened_file)
+        counted_characters = character_counter(opened_file)
+        # print(counted_characters)
+        sorted_list = sorted_char_counter(counted_characters)
+        print("============ BOOKBOT ============")
+        print(f"Analyzing book found at {book_path}...")
+        print("----------- Word Count ----------")
+        print(f"Found {split_string} total words")
+        print("--------- Character Count -------")
 
-    for i in sorted_list:
-        if i["char"].isalpha():
-            print(f"{i['char']}: {i['num']}")
+        for i in sorted_list:
+            if i["char"].isalpha():
+                print(f"{i['char']}: {i['num']}")
 
-    print("============= END ===============")
+        print("============= END ===============")
 
 
 main()
